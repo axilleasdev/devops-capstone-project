2c823cd0e9e22f33b3666162d4b138408670edc0
axilleasdev <axilleaskarantzas@gmail.com>
2024-02-18 07:54:44 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -24,6 +24,7 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -36,7 +37,6 @@ class TestAccountService(TestCase):
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
         talisman.force_https = False
-
 
     @classmethod
     def tearDownClass(cls):
@@ -186,13 +186,12 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(len(data), 5)
 
-
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    # SEACURITY TEST 
+    # SEACURITY TEST
 
     def test_security_headers(self):
         """It should return security headers"""
@@ -207,7 +206,7 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-    # TEST CORS 
+    # TEST CORS
 
     def test_cors_security(self):
         """It should return a CORS header"""
